840625160df3e004181e231e11bd5d06dd88eb41
renatoduttra <renatoduttra@gmail.com>
2022-04-05 00:42:03 -0300

    Estrutura do socket
    Descrevi todos os passos para facilitar a criação do programa

---
+++ TCP-IP/Cliente-tcp.py
@@ -0,0 +1,8 @@
+#Sequência de funcionamento do Cliente TCP
+
+#Caso forneçamos um nome de um servidor hospedeiro, converter em endereço IP;
+#Caso forneçamos um nome de protocolo de transporte, converter em número;
+#Criar o socket (função socket);
+#Conectar com o servidor (função connect);
+#Enviar/Receber dados (permanecer nesse passo enquanto tiver dados para enviar/receber);
+#Fechar o socket.
